0d30f34bb4acccd0379a5598d2859a5117eb1327
Sakshee Agrawal <109152734+sakshee15@users.noreply.github.com>
2023-09-13 13:21:23 +0530

    Time: 21 ms (37.33%), Space: 17.7 MB (47.6%) - LeetHub

--- 0135-candy/0135-candy.cpp
+++ 0135-candy/0135-candy.cpp
@@ -9,18 +9,19 @@ public:
 	 for (int i = 1; i < size; i++)
 	 {
 		 if(ratings[i]>ratings[i-1])
-			 num[i]=num[i-1]+1;
+			 num[i]=num[i-1]+1; 
+
 	 }
-	 for (int i= size-1; i>0 ; i--)
-	 {
-		 if(ratings[i-1]>ratings[i])
-			 num[i-1]=max(num[i]+1,num[i-1]);
-	 }
-	 int result=0;
-	 for (int i = 0; i < size; i++)
-	 {
-		 result+=num[i];
-	 }
+    for (int i = size - 2; i >= 0; i--) {
+            if (ratings[i] > ratings[i + 1] && num[i] <= num[i + 1])
+                num[i] = num[i + 1] + 1;
+    }
+     int result=0;
+     for(int i=0;i<num.size();i++){
+         result+=num[i];
+     }
+    
+	
 	 return result;
  }
 };
