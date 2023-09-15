dd193bd0f3c81a3f3048fa3f82169a927aeae317
Sakshee Agrawal <109152734+sakshee15@users.noreply.github.com>
2023-09-15 08:05:54 +0530

    Time: 3 ms (99.5%), Space: 13.6 MB (58.91%) - LeetHub

---
+++ 2308-divide-array-into-equal-pairs/2308-divide-array-into-equal-pairs.cpp
@@ -0,0 +1,10 @@
+class Solution {
+public:
+    bool divideArray(vector<int>& nums) {
+        sort(nums.begin(),nums.end());
+        for(int i=0;i<=nums.size()-1;i+=2){
+            if(nums[i]!=nums[i+1]) return false;
+        }
+        return true;
+    }
+};
