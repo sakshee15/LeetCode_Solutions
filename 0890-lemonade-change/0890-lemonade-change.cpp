4e7138392344b064f98f68b67117c377942da4e4
Sakshee Agrawal <109152734+sakshee15@users.noreply.github.com>
2023-09-15 08:19:11 +0530

    Time: 84 ms (45.36%), Space: 83.8 MB (7%) - LeetHub

--- 0890-lemonade-change/0890-lemonade-change.cpp
+++ 0890-lemonade-change/0890-lemonade-change.cpp
@@ -1,31 +1,29 @@
 class Solution {
 public:
     bool lemonadeChange(vector<int>& bills) {
-        unordered_map<int,int> map={{5,0},{10,0},{20,0}};
-        for(int i=0;i<bills.size();i++){
-            if(bills[i]==5){
-              map[5]++;
-            }else if(bills[i]==10){
-              if(map[5]<=0){
-                  return false;
-              }else{
-                  map[5]--;
-                  map[10]++;
-              }
-            }else{
-              if(map[5]>=1 && map[10]>=1){
-                  map[5]--;
-                  map[10]--;
-                  map[20]++;
-              }else if(map[5]>=3){
-                  map[5]-=3;
-                  map[20]++;
-              }else {
-                  return false;
-              }
+        int countFive = 0;
+        int countTen = 0;
+
+        for (const int bill : bills) {
+            if (bill == 5) {
+                countFive++;
+            } else if (bill == 10) {
+                if (countFive <= 0) {
+                    return false;
+                }
+                countFive--;
+                countTen++;
+            } else {  // $20 bill
+                if (countTen >= 1 && countFive >= 1) {
+                    countTen--;
+                    countFive--;
+                } else if (countFive >= 3) {
+                    countFive -= 3;
+                } else {
+                    return false;
+                }
             }
         }
         return true;
-
     }
 };
