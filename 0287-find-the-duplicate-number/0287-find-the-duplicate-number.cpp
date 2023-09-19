ddb05dd1d96bd036260a07e905dd559c9a0a929c
Sakshee Agrawal <109152734+sakshee15@users.noreply.github.com>
2023-09-19 09:57:02 +0530

    Time: 115 ms (37.17%), Space: 61.5 MB (30.42%) - LeetHub

--- 0287-find-the-duplicate-number/0287-find-the-duplicate-number.cpp
+++ 0287-find-the-duplicate-number/0287-find-the-duplicate-number.cpp
@@ -1,13 +1,12 @@
 class Solution {
 public:
     int findDuplicate(vector<int>& nums) {
-        unordered_map<int,int> s;
-        for(int i=0;i<nums.size();i++){
-            s[nums[i]]++;
-        }
-        for(auto &it: s){
-            if(it.second>=2) return it.first;
-        }
-        return 0;
+       sort(nums.begin(),nums.end());
+       for(int i=1;i<nums.size();i++){
+           if(nums[i]==nums[i-1]){
+               return nums[i];
+           }
+       }
+       return 0;
     }
 };
