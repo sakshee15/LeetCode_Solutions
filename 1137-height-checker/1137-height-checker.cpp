2c58bbfa9abfa13d30489f9be549ceb0cef8b199
Sakshee Agrawal <109152734+sakshee15@users.noreply.github.com>
2023-10-04 17:20:34 +0530

    Time: 0 ms (100%), Space: 8.4 MB (29.85%) - LeetHub

---
+++ 1137-height-checker/1137-height-checker.cpp
@@ -0,0 +1,12 @@
+class Solution {
+public:
+    int heightChecker(vector<int>& heights) {
+        vector<int> h=heights;
+        sort(heights.begin(),heights.end());
+        int count=0;
+        for(int i=0;i<heights.size();i++){
+            if(heights[i]!=h[i]) count++;
+        }
+        return count;
+    }
+};
