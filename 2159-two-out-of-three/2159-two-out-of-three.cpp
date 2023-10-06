8dcb6e24b1ec714c592af071f786e5565a32c683
Sakshee Agrawal <109152734+sakshee15@users.noreply.github.com>
2023-10-06 08:39:33 +0530

    Time: 19 ms (40.52%), Space: 28.7 MB (30.15%) - LeetHub

--- 2159-two-out-of-three/2159-two-out-of-three.cpp
+++ 2159-two-out-of-three/2159-two-out-of-three.cpp
@@ -3,30 +3,12 @@ public:
     vector<int> twoOutOfThree(vector<int>& nums1, vector<int>& nums2, vector<int>& nums3) {
         unordered_map<int,int> map;
         vector<int> ans;
-        sort(nums1.begin(),nums1.end());
-        sort(nums2.begin(),nums2.end());
-        sort(nums3.begin(),nums3.end());
-        int prev=0;
-        for(int i:nums1){
-            if(prev!=i){
-                map[i]++;
-                prev=i;
-            }
-        }
-        prev=0;
-        for(int i:nums2){
-            if(prev!=i){
-                map[i]++;
-                prev=i;
-            }
-        }
-        prev=0;
-        for(int i:nums3){
-            if(prev!=i){
-                map[i]++;
-                prev=i;
-            }
-        }
+        unordered_set<int> s1(nums1.begin(),nums1.end());
+        unordered_set<int> s2(nums2.begin(),nums2.end());
+        unordered_set<int> s3(nums3.begin(),nums3.end());
+        for(int i:s1) map[i]++;
+        for(int i:s2) map[i]++;
+        for(int i:s3) map[i]++;
         for(auto &i:map){
             if(i.second>=2){
                 ans.push_back(i.first);
