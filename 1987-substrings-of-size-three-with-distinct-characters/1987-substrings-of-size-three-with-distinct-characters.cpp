0ea8d36ea14073ffbe45d1cc4eb998049a9cf14f
Sakshee Agrawal <109152734+sakshee15@users.noreply.github.com>
2023-10-06 08:28:00 +0530

    Time: 1 ms (63.16%), Space: 6.5 MB (52.61%) - LeetHub

--- 1987-substrings-of-size-three-with-distinct-characters/1987-substrings-of-size-three-with-distinct-characters.cpp
+++ 1987-substrings-of-size-three-with-distinct-characters/1987-substrings-of-size-three-with-distinct-characters.cpp
@@ -1,17 +1,13 @@
 class Solution {
 public:
     int countGoodSubstrings(string s) {
-        int count=0;
-        if(s.size()<3) return 0;
-        char a=s[0];
-        char b=s[1];
-        char c=s[2];
-        if(a!=b && b!=c && c!=a) count++;
-        for(int i=3;i<s.size();i++){
-            a=b;
-            b=c;
-            c=s[i];
-            if(a!=b && b!=c && c!=a) count++;
+        int count = 0;
+        if (s.size() < 3) return 0;
+
+        for (int i = 2; i < s.size(); i++) {
+            if (s[i] != s[i - 1] && s[i] != s[i - 2] && s[i - 1] != s[i - 2]) {
+                count++;
+            }
         }
 
         return count;
