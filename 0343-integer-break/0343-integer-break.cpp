c8d2d3f77460371f84fdc9c18be040cdb427a506
Sakshee Agrawal <109152734+sakshee15@users.noreply.github.com>
2023-10-06 08:03:47 +0530

    Time: 2 ms (49.96%), Space: 6.7 MB (11.85%) - LeetHub

--- 0343-integer-break/0343-integer-break.cpp
+++ 0343-integer-break/0343-integer-break.cpp
@@ -4,13 +4,16 @@ public:
         if (n <= 3) {
             return n - 1;
         }
-        
-        int ans = 1;
-        while (n > 4) {
-            ans *= 3;
-            n -= 3;
+
+        int quotient = n / 3;
+        int remainder = n % 3;
+
+        if (remainder == 0) {
+            return pow(3, quotient);
+        } else if (remainder == 1) {
+            return pow(3, quotient - 1) * 4;
+        } else {
+            return pow(3, quotient) * 2;
         }
-        
-        return ans * n;
     }
 };
