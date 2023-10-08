e3620f39df653718b3750f600c4b0565867f9787
Sakshee Agrawal <109152734+sakshee15@users.noreply.github.com>
2023-10-08 18:58:26 +0530

    Time: 16 ms (15.2%), Space: 15.5 MB (6.31%) - LeetHub

--- 2416-evaluate-boolean-binary-tree/2416-evaluate-boolean-binary-tree.cpp
+++ 2416-evaluate-boolean-binary-tree/2416-evaluate-boolean-binary-tree.cpp
@@ -12,12 +12,13 @@
 class Solution {
 public:
     bool evaluateTree(TreeNode* root) {
-        if(root->val==2){
-            return evaluateTree(root->left) | evaluateTree(root->right);
-        }else if(root->val == 3){
-             return evaluateTree(root->left) & evaluateTree(root->right); 
+
+        if (root->val == 2) {
+            return evaluateTree(root->left) || evaluateTree(root->right);
+        } else if (root->val == 3) {
+            return evaluateTree(root->left) && evaluateTree(root->right);
+        } else {
+            return root->val;
         }
-        else return root->val;
-        
     }
 };
