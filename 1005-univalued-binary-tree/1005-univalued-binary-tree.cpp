743838942e9c26515f50ab6511edd7a95b9a402a
Sakshee Agrawal <109152734+sakshee15@users.noreply.github.com>
2023-10-10 08:38:26 +0530

    Time: 0 ms (100%), Space: 10.3 MB (18.96%) - LeetHub

--- 1005-univalued-binary-tree/1005-univalued-binary-tree.cpp
+++ 1005-univalued-binary-tree/1005-univalued-binary-tree.cpp
@@ -9,37 +9,37 @@
  *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
  * };
  */
-// class Solution {
-// public:
-//     bool isUnivalTree(TreeNode* root) {
-//         int uniVal=root->val;
-//         queue<TreeNode*> q;
-//         q.push(root);
-//         while(!q.empty()){
-//             TreeNode* temp = q.front();
-//             q.pop();
-//             if(temp->val!=uniVal) return false;
-//             if(temp->left) q.push(temp->left);
-//             if(temp->right) q.push(temp->right);
-//         }
-//         return true;
-//     }
-// };
 class Solution {
 public:
     bool isUnivalTree(TreeNode* root) {
-        return isUnival(root, root->val);
-    }
-    
-    bool isUnival(TreeNode* node, int targetVal) {
-        if (node == nullptr) {
-            return true;
+        int uniVal=root->val;
+        queue<TreeNode*> q;
+        q.push(root);
+        while(!q.empty()){
+            TreeNode* temp = q.front();
+            q.pop();
+            if(temp->val!=uniVal) return false;
+            if(temp->left) q.push(temp->left);
+            if(temp->right) q.push(temp->right);
         }
-        
-        if (node->val != targetVal) {
-            return false;
-        }
-        
-        return isUnival(node->left, targetVal) && isUnival(node->right, targetVal);
+        return true;
     }
 };
+// class Solution {
+// public:
+//     bool isUnivalTree(TreeNode* root) {
+//         return isUnival(root, root->val);
+//     }
+    
+//     bool isUnival(TreeNode* node, int targetVal) {
+//         if (node == nullptr) {
+//             return true;
+//         }
+        
+//         if (node->val != targetVal) {
+//             return false;
+//         }
+        
+//         return isUnival(node->left, targetVal) && isUnival(node->right, targetVal);
+//     }
+// };
