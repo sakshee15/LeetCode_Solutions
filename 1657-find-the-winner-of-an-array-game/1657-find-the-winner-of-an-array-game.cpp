24895b0514aea053b17401d29cf1f3998dc38076
Sakshee Agrawal <109152734+sakshee15@users.noreply.github.com>
2023-11-05 10:00:38 +0530

    Time: 99 ms (22.68%), Space: 63.4 MB (56.48%) - LeetHub

---
+++ 1657-find-the-winner-of-an-array-game/1657-find-the-winner-of-an-array-game.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    int getWinner(vector<int>& arr, int k) {
+        int consecutiveWins = 0;
+        int currentWinner = arr[0];
+
+        for (int i = 1; i < arr.size(); i++) {
+            if (arr[i] > currentWinner) {
+                currentWinner = arr[i];
+                consecutiveWins = 1;
+            } else {
+                consecutiveWins++;
+            }
+
+            if (consecutiveWins == k) {
+                return currentWinner;
+            }
+        }
+
+        return currentWinner;
+    }
+};
